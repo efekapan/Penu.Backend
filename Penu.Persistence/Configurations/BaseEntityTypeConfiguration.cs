e122243c88017d735e7747df7e3af352e63da2f6
Efe Kapan <efekapan@hotmail.com>
2025-07-04 20:46:28 +0300

    Create BaseEntityTypeConfiguration.cs

---
+++ Penu.Persistence/Configurations/BaseEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Penu.Domain.Common;
+
+namespace Penu.Persistence.Configurations
+{
+    public class BaseEntityTypeConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(e => e.Id);
+        }
+    }
+}
