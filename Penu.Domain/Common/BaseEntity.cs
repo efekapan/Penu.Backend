b62a031c6e643b3cb02f720e0d92c4868aed6748
Efe Kapan <efekapan@hotmail.com>
2025-07-04 18:55:54 +0300

    Update BaseEntity.cs

--- Penu.Domain/Common/BaseEntity.cs
+++ Penu.Domain/Common/BaseEntity.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedDate { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
 
     }
 }
